b51b3c3abf75ede2e5da21443f28c981c69a5f9c
prt-prmr <preetparmar@outlook.com>
2020-02-03 17:05:43 -0500

    Create Search Console.py

---
+++ Search Console.py
@@ -0,0 +1,277 @@
+# Importing Libraries
+import tkinter as tk
+import time
+import pickle
+import os
+import pandas as pd
+from datetime import datetime, timedelta
+from google_auth_oauthlib.flow import InstalledAppFlow
+from apiclient.discovery import build
+from tkinter import ttk
+from tkinter import messagebox
+
+
+class MyGUI(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.initialize_tkinter_window()
+        self.create_upper_frame()
+        self.create_lower_frame()
+        self.connect_to_search_console()
+
+    def connect_to_search_console(self):
+        """
+        Function to connect to Search Console.
+        Function looks for the client_secret in the folder, if it's not present then goes through the authentication process
+        """
+        self.update_output('Connecting to Search Console....')
+        self.client_secret_location = 'C:\My Files\Python Projects\Client Secrets\Search Console Config\credentials.pickle'
+        self.start_progress_bar()
+        self.oauth_scope = ('https://www.googleapis.com/auth/webmasters.readonly', 'https://www.googleapis.com/auth/webmasters')
+
+        # Building Connection
+        try:
+            self.credentials = pickle.load(open(self.client_secret_location, "rb"))
+        except (OSError, IOError) as e:
+            self.flow = InstalledAppFlow.from_client_secrets_file("client_secrets.json", scopes=self.oauth_scope)
+            self.credentials = self.flow.run_console()
+            pickle.dump(self.credentials, open("config/credentials.pickle", "wb"))
+
+        # Connect to Search Console Service using the credentials
+        self.webmasters_service = build('webmasters', 'v3', credentials=self.credentials)
+        self.update_output('Connected to Search Console')
+
+    def initialize_tkinter_window(self):
+        """
+        Function to initialize the Window and it's properties.
+        """
+        self.width, self.height = 500, 600  # Screen size of the initial window
+        self.ws = self.winfo_screenwidth()  # width of the screen
+        self.hs = self.winfo_screenheight()  # height of the screen
+
+        # calculate x and y coordinates for the Tk root window
+        self.x = (self.ws / 2) - (self.width / 2)
+        self.y = (self.hs / 2) - (self.height / 2)
+        self.geometry('%dx%d+%d+%d' % (self.width, self.height, self.x, self.y))
+
+        self.title("Sam Edelman Search Console Data Extract")
+        self.canvas = tk.Canvas(self, bg='#D6EAF8')
+        self.canvas.place(relwidth=1, relheight=1)
+
+        self.exit_button = tk.Button(self, text='Exit', command=self.exit_application)
+        self.exit_button.place(relx=0.5, rely=0.97, relwidth=0.15, relheight=0.05, anchor='s')
+
+        self.progress_bar = ttk.Progressbar(self, orient='horizontal', length=286, mode='determinate')
+        self.progress_bar.place(relx=0.5, rely=0.44, relwidth=0.75, relheight=0.05, anchor='n')
+
+    def create_upper_frame(self):
+        """
+        Function to define the top frame and it's widgets
+        """
+        self.frame_1 = tk.Frame(self, bg='#5DADE2', bd=5)
+        self.frame_1.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.25, anchor='n')
+
+        self.url_label = tk.Label(self.frame_1, text='Custom URL:', justify='left', anchor='w')
+        self.url_label.place(relx=0, rely=0, relwidth=0.43, relheight=0.22)
+        self.url_entry = tk.StringVar()
+        self.url_entry = tk.Entry(self.frame_1, bd=1, textvariable=self.url_entry)
+        self.url_entry.place(relx=0.45, rely=0, relwidth=0.54, relheight=0.22)
+
+        self.start_date_label = tk.Label(self.frame_1, text='Start Date (YYYY-MM-DD):', justify='left', anchor='w')
+        self.start_date_label.place(relx=0, rely=0.25, relwidth=0.43, relheight=0.22)
+        self.start_date_entry = tk.StringVar()
+        self.start_entry = tk.Entry(self.frame_1, bd=1, textvariable=self.start_date_entry)
+        self.start_entry.place(relx=0.45, rely=0.25, relwidth=0.54, relheight=0.22)
+
+        self.end_date_label = tk.Label(self.frame_1, text='End Date (YYYY-MM-DD):', justify='left', anchor='w')
+        self.end_date_label.place(relx=0, rely=0.50, relwidth=0.43, relheight=0.22)
+        self.end_date_entry = tk.StringVar()
+        self.end_entry = tk.Entry(self.frame_1, bd=1, textvariable=self.end_date_entry)
+        self.end_entry.place(relx=0.45, rely=0.50, relwidth=0.54, relheight=0.22)
+
+        self.submit_button = tk.Button(self.frame_1, text='Submit', command=self.get_data)
+        self.submit_button.place(relx=0.5, rely=0.80, relwidth=0.30, relheight=0.20, anchor='n')
+
+    def create_lower_frame(self):
+        """
+        Function to define lower frame and it's widgets
+        """
+        self.frame_2 = tk.Frame(self, bg='#5DADE2', bd=5)
+        self.frame_2.place(relx=0.5, rely=0.45, relwidth=0.75, relheight=0.45, anchor='n')
+        self.output_text = tk.StringVar()
+        self.output_label = tk.Label(self.frame_2, textvariable=self.output_text, anchor='nw', justify='left')
+        self.output_label.place(relwidth=1, relheight=1)
+
+    def date_range(self, start_date, end_date, delta=timedelta(days=1)):
+        """
+            The range is inclusive, so both start_date and end_date will be returned
+            Args:
+                start_date: The datetime object representing the first day in the range.
+                end_date: The datetime object representing the second day in the range.
+                delta: A datetime.timedelta instance, specifying the step interval. Defaults to one day.
+            Yields:
+                Each datetime object in the range.
+            """
+        self.current_date = start_date
+        while self.current_date <= end_date:
+            yield self.current_date
+            self.current_date += delta
+
+    def get_data(self):
+        """
+        Fetches the user provided data in the text labels and exports data from Search Console
+        :return:
+        """
+        self.output_text.set(' ')
+        # self.start_progress_bar()
+        self.max_rows = 25000
+        self.i = 0
+        self.output_rows = []
+        self.start_date_text = self.start_date_entry.get()
+        self.end_date_text = self.end_date_entry.get()
+        self.site_url = self.url_entry.get()  # https://www.samedelman.com/
+        self.proceed = self.check_file()
+        if self.proceed:
+            self.request_data()
+        else:
+            pass
+
+    def request_data(self):
+        """
+        Requests data from Search Console
+        """
+        try:
+            self.start_date = datetime.strptime(self.start_date_text, "%Y-%m-%d")
+            self.end_date = datetime.strptime(self.end_date_text, "%Y-%m-%d")
+        except ValueError:
+            messagebox.showerror('Error', 'Date value should be YYYY-MM-DD')
+            return
+        if self.start_date > self.end_date:
+            messagebox.showerror('Error', 'Start Date can not be greater than the End Date')
+            return
+        else:
+            pass
+        for self.date in self.date_range(self.start_date, self.end_date):
+            self.date = self.date.strftime("%Y-%m-%d")
+            print(self.date)
+            self.update_output('Fetching Data for: %s' % self.date)
+            i = 0
+            while True:
+                self.start_progress_bar()
+                self.request = {
+                    'startDate': self.date,
+                    'endDate': self.date,
+                    'dimensions': ["query", "page"],
+                    "searchType": "Web",
+                    'rowLimit': self.max_rows,
+                    'startRow': self.i * self.max_rows
+                }
+
+                self.response = self.webmasters_service.searchanalytics().query(siteUrl=self.site_url, body=self.request).execute()
+                print()
+                if self.response is None:
+                    # print("there is no response")
+                    messagebox.showerror('Error', 'No Response from Search Console')
+                    break
+                if 'rows' not in self.response:
+                    print("row not in response")
+                    self.update_output('Less than 25000 rows for %s' % self.date)
+                    break
+                else:
+                    for self.row in self.response['rows']:
+                        self.keyword = self.row['keys'][0]
+                        self.page = self.row['keys'][1]
+                        self.output_row = [self.date, self.keyword, self.page, self.row['clicks'], self.row['impressions'], self.row['ctr'], self.row['position']]
+                        self.output_rows.append(self.output_row)
+                    self.i = self.i + 1
+        self.start_progress_bar()
+        self.df = pd.DataFrame(self.output_rows, columns=['date', 'query', 'page', 'clicks', 'impressions', 'ctr', 'avg_position'])
+        self.export_df()
+        # self.stop_progress_bar()
+
+    def check_file(self):
+        """
+        Checks whether the file is already present in the desired folder
+        :return:
+            True -> File is not present
+            False -> File is present
+        """
+        self.folder = 'P:\Clients\Sam Edelman\Analytics\Data\Search Console'
+        self.file = 'SE_Search_%s_%s.xlsx' % (self.start_date_text, self.end_date_text)
+        self.file_name = os.path.join(self.folder, self.file)
+        self.all_files = os.listdir(self.folder)
+        if self.file not in self.all_files:
+            return True
+        else:
+            self.duplicate_box = messagebox.askquestion('Data already present', message='Would you like to replace the data?')
+            if self.duplicate_box == 'yes':
+                return True
+            else:
+                return False
+
+    def export_df(self):
+        """
+        Exports the report into an excel file in a desired format
+        """
+        self.folder = 'P:\Clients\Sam Edelman\Analytics\Data\Search Console'
+        self.file = 'SE_Search_%s_%s.xlsx' % (self.start_date_text, self.end_date_text)
+        self.rows, self.columns = self.df.shape
+        self.file_name = os.path.join(self.folder, self.file)
+        self.all_files = os.listdir(self.folder)
+        # if self.file not in self.all_files:
+        self.update_output('Exporting to an Excel File')
+        self.start_progress_bar()
+        self.df.to_excel(self.file_name, sheet_name='Data', index=False)
+        self.text = 'File: %s was exported. \nSaved here: %s \nTotal no. of Rows and Columns = %s, %s' % (self.file, self.folder, self.rows, self.columns)
+        self.update_output(self.text)
+        # else:
+        #     self.duplicate_box = messagebox.askquestion('Data already present', message='Would you like to replace the data?')
+        #     if self.duplicate_box == 'yes':
+        #         self.update_output('Exporting to an Excel File')
+        #         self.start_progress_bar()
+        #         self.df.to_excel(self.file_name, sheet_name='Data', index=False)
+        #         self.text = 'File: %s was exported. \nSaved here: %s \nTotal no. of Rows and Columns = %s, %s\n \n \n' % (self.file, self.folder, self.rows, self.columns)
+        #         self.update_output(self.text)
+        #     else:
+        #         pass
+
+    def exit_application(self):
+        """
+        Question box for exiting the application
+        """
+        self.exit_box = messagebox.askquestion('Exit Application', 'Are you sure you want to exit the application', icon='warning')
+        if self.exit_box == 'yes':
+            self.destroy()
+        else:
+            pass
+
+    def update_output(self, text: str):
+        """
+        Updates the output label and adds in a blank line
+        :param text: String text to add in to the output label
+        """
+        self.current_text = self.output_text.get()
+        self.current_text = '%s\n%s' % (self.current_text, text)
+        self.output_text.set(self.current_text)
+
+    def start_progress_bar(self):
+        """
+        Starts the progress bar
+        """
+        self.progress_bar['maximum'] = 10
+        for i in range(11):
+            time.sleep(0.05)
+            self.progress_bar['value'] = i
+            self.progress_bar.update()
+        self.progress_bar['value'] = 0
+
+    def stop_progress_bar(self):
+        """
+        Stops the progress bar
+        """
+        self.progress_bar.stop()
+
+
+app = MyGUI()
+app.mainloop()
